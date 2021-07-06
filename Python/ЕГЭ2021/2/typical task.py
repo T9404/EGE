87067d6c5928fd836a7201292b65c3383bb92b12
tomcat <qananir@mail.ru>
2021-07-06 20:28:41 +0500

    2, 8

---
+++ Python/ЕГЭ2021/2/typical task.py
@@ -0,0 +1,22 @@
+# В данном задании нет ничего сложного, нужно лишь переписать формулу :)
+print('№ 4146')
+print('>>>>>')
+print('a b c d')
+for a in range(2):
+    for b in range(2):
+        for c in range(2):
+            for d in range(2):
+                if (a <= b) and (not (b == c)) and (d <= a):
+                    print(a, b, c, d)
+print('Вам осталось сопоставить таблицу истинности')
+
+# Короткий способ:
+
+from itertools import product
+# см. '../ЕГЭ2021/itertools/learn'
+
+print('>>>>>')
+print('a b c d')
+for a, b, c, d in product([0, 1], repeat=4):
+    if (a <= b) and (not (b == c)) and (d <= a):
+        print(a, b, c, d)
---
+++ Python/ЕГЭ2021/8/typical task.py
@@ -0,0 +1,1 @@
+from itertools import *
