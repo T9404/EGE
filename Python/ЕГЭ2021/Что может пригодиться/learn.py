92cecb3184486053f6bede355e1f36bd09f7b4b4
tomcat <69140932+T9404@users.noreply.github.com>
2021-07-11 21:40:34 +0500

    Update learn.py

--- Python/ЕГЭ2021/Что может пригодиться/learn.py
+++ Python/ЕГЭ2021/Что может пригодиться/learn.py
@@ -2,3 +2,6 @@
 Дополнительные фишки питона, которые могут быть полезными
 https://pavel-karateev.gitbook.io/intermediate-python/funkcionalnoe-programmirovanie/lambdas
 '''
+
+#Нахождение суммы цифр числа
+summa = sum(map(int, str(input())))
