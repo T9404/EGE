cb12509f4e5b778cd993c6726c5628422ddb2f1d
tomcat <69140932+T9404@users.noreply.github.com>
2021-07-14 22:44:29 +0500

    Update learn.py

--- Python/ЕГЭ2021/Что может пригодиться/learn.py
+++ Python/ЕГЭ2021/Что может пригодиться/learn.py
@@ -1,6 +1,10 @@
 '''
 Дополнительные фишки питона, которые могут быть полезными
 https://pavel-karateev.gitbook.io/intermediate-python/funkcionalnoe-programmirovanie/lambdas
+
+Системы СС и о них
+https://ege-study.ru/ru/ege/materialy/informatika/zadacha-16-razbor-razlichnyx-tipov-zadach/
+
 '''
 
 #Нахождение суммы цифр числа
