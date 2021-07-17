c7401a87fc2a3d9a926f33d665394fc7d19ad683
tomcat <69140932+T9404@users.noreply.github.com>
2021-07-17 10:44:51 +0500

    new

--- Python/ЕГЭ2021/Что может пригодиться/learn.py
+++ Python/ЕГЭ2021/Что может пригодиться/learn.py
@@ -1,15 +1,33 @@
 '''
-Дополнительные фишки питона, которые могут быть полезными
+1) Дополнительные фишки питона, которые могут быть полезными
 https://pavel-karateev.gitbook.io/intermediate-python/funkcionalnoe-programmirovanie/lambdas
 
-Системы СС и о них
+2) Системы СС и о них
 https://ege-study.ru/ru/ege/materialy/informatika/zadacha-16-razbor-razlichnyx-tipov-zadach/
 
 '''
 
-#Нахождение суммы цифр числа
+# 3) Нахождение суммы цифр числа
 summa = sum(map(int, str(input())))
 
-#Что такое длина отрезка и количество точек в нем?
+
+# 4) Что такое длина отрезка и количество точек в нем?
 #Длина: конец-начало
 #Количество: конец-начало+1
+
+
+# 5) Find в python
+dring = 'gcGC'
+print('gcGC'.find(dring)) #если строка состоит из 'gcGC' то выведет индекс найденного элемента, вывел 0
+
+word = 'gcGCgcGCgcGCgcGCfdsfgcGC'
+print('gcGC'.find(word)) #вывел -1, т.к. не нашел элемент 
+
+dr = 'gcGC dased gcGC'
+print('gcGC'.find(dr)) # вывел -1, т.к. строка состоит не только из одной комбинации gcGC
+
+
+# 6) Count в python
+string = '88005553535прощепозвонитьчемукого-тозанимать'
+print(string.count('0')) #вернет 2, т.к. два нуля в строке. Лучше не играть с этим в 24 задании в подстроках, т.к. теряет. он проходит через строку, а не через элемент
+print(string.count('5')) # вернет 5
