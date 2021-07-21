ae034bde05d991b7ef08a1a90152eab80d64f8c6
tomcat <69140932+T9404@users.noreply.github.com>
2021-07-21 22:52:16 +0500

    мчс

---
+++ Python/ЕГЭ2021/27/Основные методы решения/МЧС.py
@@ -0,0 +1,40 @@
+#Метод частичных сумм
+#К.Ю.Поляков 2670
+f = open('27-10a.txt')
+n = int(f.readline())
+
+def get():# функция для считывание тройки
+    return list(map(int, f.readline().split()))
+new_list = get() #считывание первой тройки(от нее будем двигаться)
+
+for _ in range(n-1): #n-1 ОБЯЗАТЕЛЬНО!
+    dinamic_list = get() # динамично считываем тройку
+
+    
+    cmb = [a+b for a in new_list for b in dinamic_list] #всевозможная генерация
+
+    comparison = [float('-inf')]*4 #делимость на 4 спрашивают
+
+    for number in cmb:
+        if number > comparison[number%4]:
+            comparison[number%4] = number
+    ''' Почему работает так? Мы берем ПРЕДЫДУЩИЕ значения и новые, складываем
+        Берем ост от деления на 4, т.к. делимость на 4 спрашивают
+        Можно подумать и использовать алг для решения проблемы делимости в другой сс.
+        например в 5 системе => % 5 
+        
+    '''
+
+    new_list = [number_new for number_new in comparison if number_new != float('-inf')] #если значения не пустые
+    #print(new_list)
+    #input()
+
+
+#можно реализовать по другому
+mina = 0
+for i in new_list:
+    if i % 4 != 0:
+        if i > mina:
+            mina = i
+        
+print(mina)
