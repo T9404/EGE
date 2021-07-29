d31a1aa877ca65fc6430ed6de3fc17f91b69e968
tomcat <69140932+T9404@users.noreply.github.com>
2021-07-29 18:51:38 +0500

    Create 2507.cpp

---
+++ C++/ЕГЭ2021/24/Решение заданий. Поляков/2507.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+
+ifstream fin("k8.txt");
+
+int main() {
+	char left, right; // два элемента для их сравнения
+
+	int cnt(1), maxcnt(1);
+	char symb;
+
+	fin.get(left);
+	while (fin.get(right)) {
+		if (left == right) cnt++;
+		else {
+			if (cnt > maxcnt) { maxcnt = cnt; symb = left; }
+			cnt = 1;
+		}
+		left = right;
+	}
+
+	cout << symb << " " << maxcnt;
+	fin.close();
+
+	return 0;
+}
