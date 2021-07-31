b21cb6699c498a0f9b0816c1f6d29d5e7a8266c1
tomcat <69140932+T9404@users.noreply.github.com>
2021-07-31 19:08:31 +0500

    Update 4240.py

--- Python/ЕГЭ2021/8/Решение заданий. Поляков/4240.py
+++ Python/ЕГЭ2021/8/Решение заданий. Поляков/4240.py
@@ -4,8 +4,9 @@
 между собой. Сколько слов, включая исходное, может составить Даша? 
 '''
 
+''' Обратите внимание, в этой задачи повторяются ТТ и КК'''
 
-
+#1 Решение
 from itertools import permutations
 a = []
 word = 'ТИКТОК'
@@ -19,3 +20,20 @@ for i in permutations(word, r=6):
         
 a1 = list(set(a))
 print(len(a1))
+
+#2 Решение
+from itertools import permutations
+a = []
+word = 'ТИКТОК'
+
+count = 0
+for i in permutations(word, r=6):
+    w = ''.join(i)
+    if ('ТТ' not in w) and ('КК' not in w):
+        count+=1
+            
+        
+print(count//4)
+
+
+
