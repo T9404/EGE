6b061e359f754e34abba6168bcd5ae07da827092
tomcat <69140932+T9404@users.noreply.github.com>
2021-08-07 18:58:36 +0500

    new 23 recursion

---
+++ C++/ЕГЭ2021/23/Решение заданий. Поляков/4183.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int recursion(int start, int end) {
+	if (start == end) {
+		return 1;
+	}
+	else if (start < end) {
+		return 0;
+	}
+	else {
+		return recursion(start - 8, end) + recursion(start / 2, end);
+	}
+}
+
+int main() {
+	cout << (recursion(102, 43) * recursion(43, 5));
+	return 0;
+}
