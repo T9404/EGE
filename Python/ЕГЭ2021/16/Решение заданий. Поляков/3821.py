ac57328e15ee2c1a3dab9655d09536bb74ebd63d
tomcat <69140932+T9404@users.noreply.github.com>
2021-08-11 10:45:08 +0500

    new 16 tasks

---
+++ Python/ЕГЭ2021/16/Решение заданий. Поляков/3821.py
@@ -0,0 +1,11 @@
+def f(n):
+    if n < 2:
+        return 1
+    elif n >= 2 and n % 3 == 0:
+        return f(n/3)-1
+    elif n >=2 and n % 3 != 0:
+        return f(n-1)+17
+n = 1
+while (not (f(n) == 110)):
+    n+=1
+print(n)
---
+++ Python/ЕГЭ2021/16/Решение заданий. Поляков/4192.py
@@ -0,0 +1,8 @@
+def f(n):
+    if n == 0:
+        return 1
+    elif (n > 0 and n % 2 == 0):
+        return f(n-1)+f(n-2)
+    elif (n > 0 and n % 2 != 0):
+        return 1.5*f(n-1)
+print(int(f(15))) #ответ 3
