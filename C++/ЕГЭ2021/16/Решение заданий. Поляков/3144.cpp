c56a8caf30d1b0a8e842df69b4f6ade4e2a2116b
tomcat <69140932+T9404@users.noreply.github.com>
2021-08-11 10:53:57 +0500

    new 16 tasks

---
+++ C++/ЕГЭ2021/16/Решение заданий. Поляков/3144.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int F(int n, int m)
+{
+	if (m == 0)
+		return 1;
+	else
+		return n * F(n, m - 1);
+}
+int main() {
+
+	int count = 0;
+
+	for (int n = 1; n <= 100000; n++) {
+		if ((F(n, 2) >= 100) and (F(n, 2) <= 1000))
+			count++;
+	}
+
+	cout << count;
+	return 0;
+}
---
+++ C++/ЕГЭ2021/16/Решение заданий. Поляков/3816.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int f(int n) {
+	if (n < 2) {
+		return 1;
+	}
+	else if ((n >= 2) and (n % 3 == 0)) {
+		return f(n / 3) - 1;
+	}
+	else if ((n >= 2) and (n % 3 != 0)) {
+		return f(n - 1) + 17;
+	}
+}
+
+
+int main() {
+
+	int count = 0;
+
+	for (int n = 1; n <= 100000; n++) {
+		if (f(n) == 43)
+			count++;
+	}
+
+	cout << count;
+	return 0;
+}
