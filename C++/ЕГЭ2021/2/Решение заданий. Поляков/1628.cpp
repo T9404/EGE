32d5ba09fc79c048e12764162d1c1635dbbae47c
tomcat <69140932+T9404@users.noreply.github.com>
2021-08-14 20:20:37 +0500

    new 2 task

---
+++ C++/ЕГЭ2021/2/Решение заданий. Поляков/1628.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool func(int x, int y, int z, int w) {
+	return (x and z) or ((w <= x) == (z <= y));
+
+}
+
+int main() {
+	cout << "x y z w F" << endl;
+
+	vector<int> number = { 0, 1 };
+
+	for (auto& x : number)
+		for (auto& y : number)
+			for (auto& z : number)
+				for (auto& w : number)
+					if (func(x, y, z, w) == false)
+						cout << x << " " << y << " " << z << " " << w << " " << "0" << endl;
+	return 0;
+}
