222ec17de5a8dbcfe731a559bc4dc30830e9121c
tomcat <69140932+T9404@users.noreply.github.com>
2021-08-19 20:26:01 +0500

    вам нравятся наши комментарии?

---
+++ Python/ЕГЭ2021/27/Решение заданий. Поляков/3150.py
@@ -0,0 +1,29 @@
+f = open('27-44b.txt')
+n = int(f.readline())
+# НЕ БУДЬТЕ ГЛУПЦАМИ!
+# 1 СТРОКА- 1 РАЗНИЦА!!!
+
+d = [0, 0, 0] # максимальная и остальные суммы 
+razn = [ ] #итоговые 2 разницы
+for _ in range(n):
+    a = sorted(list(map(int, f.readline().split())), reverse=True) #по убыванию 
+    for i in range(3):
+        d[i]+=a[i] # добавляем к кучкам
+
+    k = [] #массив разностей
+
+    if a[0] % 2 != a[1] % 2:
+        k.append(abs(a[0]-a[1]))
+    if a[0] % 2 != a[2] % 2:
+        k.append(abs(a[0]-a[2]))
+     # добавили все разности, дальше проверяем
+    if len(k) == 2: 
+        razn.append(min(k)) #минимальную из 2
+    elif len(k) == 1: 
+        razn.append(k[0]) #просто разницу
+    else:
+        pass #ничего
+    razn.sort() #сортируем по возрастанию все разницы
+    razn = razn[:2] #удаляем мусор
+     
+print(d[0]-sum(razn)) #ответ
