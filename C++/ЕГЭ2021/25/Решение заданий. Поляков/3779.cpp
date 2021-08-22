8d8a38337e806ab372900791ab08a88d22852003
tomcat <69140932+T9404@users.noreply.github.com>
2021-08-22 10:06:19 +0500

    speed

---
+++ C++/ЕГЭ2021/25/Решение заданий. Поляков/3779.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>  
+#include <cmath>
+#include <algorithm>
+using namespace std;
+
+
+
+int f(int x) {
+
+    int sqr = sqrt(x); int k = 0; // счетчик
+    int  del = 0; // макс делитель
+
+    for (int i = 1; i <= sqr; i++) {
+        if (x % i == 0) {
+            if ((i % 2) != 0) {
+                k++;
+                del = max(del, i);
+            }
+            if ((x / i) % 2 != 0) {
+                k++;
+                del = max(del, x / i);
+            }
+        }
+    }
+
+    if (k == 5) 
+        return del;
+    else
+        return false;
+}
+
+
+int main()  // за 2.5 мин нашел предпоследний делитель, 4.5 мин отработал полностью) 
+{
+    for (int i = 78000000; i <= 85000000; i++) {
+        if (f(i)) {
+            cout << i << " " << f(i) << endl;
+        }
+    }
+
+    return 0;
+}
