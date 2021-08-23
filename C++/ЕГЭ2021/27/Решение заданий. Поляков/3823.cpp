e7578fcaac69f36e46ce895f42d578035fbbdb7e
tomcat <69140932+T9404@users.noreply.github.com>
2021-08-23 17:46:38 +0500

    Create 3823.cpp

---
+++ C++/ЕГЭ2021/27/Решение заданий. Поляков/3823.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+int main() {
+	int n, x;
+	fstream f("C:\\Users\\admin\\Downloads\\27-59b.txt");
+	f >> n;
+
+	vector<int> count = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+	for (int i = 0; i < n; i++) {
+		f >> x;
+		vector<int> neww = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+
+
+		for (int j = 0; j < 10; j++) {
+			neww[(x + j) % 10] += count[(j)]; // суммируем кол-во пар с этим числом
+		}
+		neww[x % 10] ++; // суммируем само число
+
+
+
+		for (int j = 0; j < 10; j++) { // запись в главный массив
+			count[j] += neww[j];
+		}
+
+
+
+
+	}
+	cout << count[5];
+
+	return 0;
+}
