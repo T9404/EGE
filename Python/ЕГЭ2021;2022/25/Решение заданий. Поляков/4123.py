6f14d3d714c5633cbfe132ae960590f431c039a2
tomcat <69140932+T9404@users.noreply.github.com>
2021-08-28 10:38:36 +0500

    Create 4123.py

---
+++ Python/ЕГЭ2021;2022/25/Решение заданий. Поляков/4123.py
@@ -0,0 +1,11 @@
+k = 0; df = 0
+for a in range(1, 5000+1):
+    for b in range(1, 5000+1):
+        c = (a * a + b* b)**0.5
+        if int(c) == c:
+            if (a <= b <= c) and (c <= 5000):
+                k+=1
+                if (a + b+ c) > df:
+                    mak = int(c)
+                    df = a + b + int(c) 
+print(k, mak)
