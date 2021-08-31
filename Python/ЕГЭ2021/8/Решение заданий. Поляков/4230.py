e60d9cdeff184fc836195d1d9d393e8461855967
Malenago <87613601+Malenago@users.noreply.github.com>
2021-08-31 14:42:29 +0300

    Create 4230.py

---
+++ Python/ЕГЭ2021/8/Решение заданий. Поляков/4230.py
@@ -0,0 +1,17 @@
+#(№ 4230) (А. Куканова) Варя составляет пятизначные числа в шестнадцатиричной системе счисления, в которых цифры расположены в порядке неубывания.
+#Сколько различных чисел может составить Варя?
+
+a=[]
+for i in range(0,16):
+    a.append(i)
+def F(n):
+    for i in range(len(n)-1):
+        if (int(n[i+1])<int(n[i])): return False
+    return True
+c=0
+from itertools import product
+for i in product(a,repeat=5):
+    if (F(i)==True) and (i[0]!=0):
+        c+=1
+        print(i)
+print(c)
