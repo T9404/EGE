37d5b0c9f356a037965104a7e46623cfb9b7c4b7
tomcat <69140932+T9404@users.noreply.github.com>
2021-09-03 20:41:13 +0500

    Create Новый тип 2022. Алексей Кабанов.py

---
+++ Python/ЕГЭ2021;2022/17/Решение заданий. Поляков/Новый тип 2022. Алексей Кабанов.py
@@ -0,0 +1,15 @@
+#зАдАчА с открытого курса
+#https://vk.com/doc18466579_608953613?hash=1b76b3eb7ac6b42203&dl=4762d7fe0023ac5615
+
+f = open('17.txt')
+n = 5000
+a = [int(f.readline()) for _ in range(n) ]
+k, mak = 0, 0
+for i in range(1, n):
+    if (a[i-1] + a[i]) % 3 == 0:
+        if (a[i-1]+a[i]) % 6 != 0:
+            if abs(a[i]*a[i-1]) % 10 == 8:
+                mak = max(mak, a[i-1]+a[i])
+                k+=1
+print(k, mak)
+
