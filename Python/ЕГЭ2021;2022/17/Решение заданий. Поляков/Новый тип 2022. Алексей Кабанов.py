298b13131b8d3d0d44365e4c4e4998d91f969fd1
tomcat <69140932+T9404@users.noreply.github.com>
2021-09-03 20:42:32 +0500

    Update Новый тип 2022. Алексей Кабанов.py

--- Python/ЕГЭ2021;2022/17/Решение заданий. Поляков/Новый тип 2022. Алексей Кабанов.py
+++ Python/ЕГЭ2021;2022/17/Решение заданий. Поляков/Новый тип 2022. Алексей Кабанов.py
@@ -1,6 +1,8 @@
 #зАдАчА с открытого курса
 #https://vk.com/doc18466579_608953613?hash=1b76b3eb7ac6b42203&dl=4762d7fe0023ac5615
 
+#почитать: https://habr.com/ru/post/421071/
+
 f = open('17.txt')
 n = 5000
 a = [int(f.readline()) for _ in range(n) ]
