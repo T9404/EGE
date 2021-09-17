42e130abf39f851de734941cc5d02c0c11693758
Malenago <87613601+Malenago@users.noreply.github.com>
2021-09-17 19:25:21 +0300

    Create 226(kompege).py

---
+++ Python/ЕГЭ2021;2022/27/Задания из kompege/226(kompege).py
@@ -0,0 +1,31 @@
+f=open('C:\\Users\\XiaoMai\\Downloads\\27-B (7).txt')
+n=int(f.readline())
+
+chet=[]
+nechet=[]
+c=0
+for _ in range(n):
+    x=int(f.readline())
+    ost=x%2
+    if (ost==0):
+        for l in chet:
+            if ((l+x)%5==0): c+=1
+        chet.append(x)
+    if (ost==1):
+        for l in nechet:
+            if ((l+x)%5==0): c+=1
+        nechet.append(x)
+
+print(c)
+
+
+
+
+
+
+
+
+
+
+
+
