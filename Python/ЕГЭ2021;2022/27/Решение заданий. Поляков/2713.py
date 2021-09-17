3190b70ca8cef616b0bbce4e8f853504e104b5b4
Malenago <87613601+Malenago@users.noreply.github.com>
2021-09-17 15:58:13 +0300

    Create 2713.py

---
+++ Python/ЕГЭ2021;2022/27/Решение заданий. Поляков/2713.py
@@ -0,0 +1,20 @@
+f=open('C:\\Users\\XiaoMai\\Downloads\\27-38b.txt')
+n=int(f.readline())
+
+a=[0]*10
+for _ in range(n):
+    a[int(f.readline())]+=1
+
+sum=0
+max_n=0
+for i in range(0,10):
+    if (a[i]!=0):
+        if (a[i]%2==0): sum+=a[i]*i
+        else:
+            sum+=(a[i]-1)*i
+            max_n=max(max_n,i)
+
+sum+=max_n
+print(sum)
+
+
