1dc1dfa6947721d190e7e14714facda7e2b2f964
tomcat <69140932+T9404@users.noreply.github.com>
2021-09-20 19:33:28 +0500

    Update 477(kompege).py

--- Python/ЕГЭ2021;2022/27/Задания из kompege/477(kompege).py
+++ Python/ЕГЭ2021;2022/27/Задания из kompege/477(kompege).py
@@ -6,11 +6,11 @@ c=0
 for _ in range(n):
     x=int(f.readline())
     ost=x%2
-    q=[d for d in a if (d%2==(1-ost))]
-    if (len(q)!=0):
-        min_q=min(q)
-        if (x>min_q):
-            c+=len(q)
+    q=[d for d in a if (d%2==(1-ost))] # d % 2 == (1 - ost)   -> только в этом случае сумма будет нечетна, если четна: != 
+    if (len(q)!=0): # если массив не пустой
+        min_q=min(q) # минимальный
+        if (x>min_q): # правый элемент - х
+            c+=len(q) # новые пары с х
     a.append(x)
 
 print(c)
