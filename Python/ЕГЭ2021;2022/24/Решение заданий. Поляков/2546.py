8e6f28a2d545452b3e204b6192437de816ade3c4
tomcat <69140932+T9404@users.noreply.github.com>
2021-09-22 20:13:33 +0500

    like

---
+++ Python/ЕГЭ2021;2022/24/Решение заданий. Поляков/2546.py
@@ -0,0 +1,20 @@
+f = open('24-j5.txt')
+s = f.readline()
+mak = 0
+k = 0
+i = 1
+while True:
+    try:
+        
+        if s[i-1] == 'K' and s[i] == 'O' and s[i+1] == 'T':
+            k+=1
+            i+=3
+        else:
+            mak = max(mak, k)
+            k = 0
+            i +=1
+    except:
+        break
+
+    
+print(mak)
