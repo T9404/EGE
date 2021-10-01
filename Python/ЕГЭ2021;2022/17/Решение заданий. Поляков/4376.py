330af6887364b9358eabc7659f49fa9692b384e2
tomcat <69140932+T9404@users.noreply.github.com>
2021-10-01 10:35:56 +0500

    Create 4376.py

---
+++ Python/ЕГЭ2021;2022/17/Решение заданий. Поляков/4376.py
@@ -0,0 +1,12 @@
+f = open('17-10.txt')
+a = [int(x) for x in f]
+
+k = sumka = 0
+
+for i in range(1, len(a)-1):
+    z = sorted([a[i-1], a[i], a[i+1]])
+    if ( z[0] ** 2 + z[1] ** 2) == (z[2]**2):
+        k+=1
+        sumka+= z[2]
+
+print(k, sumka)
