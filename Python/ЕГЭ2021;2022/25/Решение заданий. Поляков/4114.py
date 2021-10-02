169e4366af9624163b9e6db1a92a6ea813f55fc6
Malenago <87613601+Malenago@users.noreply.github.com>
2021-10-02 18:18:31 +0300

    Create 4114.py

---
+++ Python/ЕГЭ2021;2022/25/Решение заданий. Поляков/4114.py
@@ -0,0 +1,18 @@
+def F(n):
+    for i in range(2,int(n**0.5)+1):
+        if (n%i==0): return False
+    return True
+
+a=[x for x in range(3,1000001)]
+c=[]
+for i in range(len(a)):
+    if (F(a[i])==False):
+        if (len(c)==0):
+            c.append(a[i-1])
+        c.append(a[i])
+    else:
+        c.append(a[i])
+        if (len(c)-2>=90): print(c[0],c[len(c)-1])
+        c=[]
+
+
