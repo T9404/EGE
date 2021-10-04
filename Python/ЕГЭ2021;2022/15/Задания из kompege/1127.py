a6e0409bf0f19aae64aea4229be70609c6dc0624
tomcat <69140932+T9404@users.noreply.github.com>
2021-10-04 12:53:26 +0500

    big commit

---
+++ Python/ЕГЭ2021;2022/15/Задания из kompege/1127.py
@@ -0,0 +1,7 @@
+def f(x, a):
+    return ( a % 7 == 0) and ( (240 % x == 0)  <= ( (a % x != 0)  <= (780 % x != 0)))
+
+for a in range(1, 10000):
+    if all( f(x, a) == 1 for x in range(1, 100000)):
+        print(a)
+        break
