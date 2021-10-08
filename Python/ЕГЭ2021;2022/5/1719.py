af65e5c5203ed13a7e669f43d2fef16f1b239042
Malenago <87613601+Malenago@users.noreply.github.com>
2021-10-08 15:46:56 +0300

    Create 1719.py

---
+++ Python/ЕГЭ2021;2022/5/1719.py
@@ -0,0 +1,11 @@
+from itertools import *
+c=0
+for i in range(300,401):
+    a=set()
+    for k in permutations(str(i),r=2):
+        s=''.join(k)
+        if (s[0]!='0'): a.add(int(s))
+    if (max(a)-min(a)==20):
+        c+=1
+
+print(c)
