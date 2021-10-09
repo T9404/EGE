d23306c5738358275e1cb23c524b3c266792fe8f
tomcat <69140932+T9404@users.noreply.github.com>
2021-10-09 21:27:34 +0500

    Update 1198.py

--- Python/ЕГЭ2021;2022/15/Задания из kompege/1198.py
+++ Python/ЕГЭ2021;2022/15/Задания из kompege/1198.py
@@ -1,21 +1,20 @@
-b = range(180, 520+10)
-c = range(160, 410+10)
+b = range(18, 52+1)
+c = range(16, 41+1)
 
 def f(x, a1, a2):
     return ( ( x in b) <= (a1 <= x <= a2) ) and ( (x not in c) or (a1 <= x <= a2))
 
 m = float('inf')
 
-for a1 in range(150, 530):
-    for a2 in range(a1+1, 530):
+for a1 in range(15, 53):
+    for a2 in range(a1+1, 53):
         if all( f(x, a1, a2) == 1 for x in range(100, 6000)):
             if m > a2- a1:
                 m = a2-a1
-                start = a1/10
-                end = a2/10
+                start = a1
+                end = a2
 
 print(start, end)
-print(m/10)
+print(m)
 
-#Про округление смотрите в ".../15/Основные методы решения/Числовая прямая.py"
 
