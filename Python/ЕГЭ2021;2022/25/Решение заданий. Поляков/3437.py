3bb88a120b2de4b0fbff0f4892bc169bdd0411fe
tomcat <69140932+T9404@users.noreply.github.com>
2021-10-14 19:51:39 +0500

    Create 3437.py

---
+++ Python/ЕГЭ2021;2022/25/Решение заданий. Поляков/3437.py
@@ -0,0 +1,21 @@
+def f(x):
+    d = set()
+    for i in range(2, int(x**0.5)+1):
+        if x % i == 0:
+            d.add(i)
+            d.add(x//i)
+    z = sorted([i for i in d])
+    if len(z) >= 2:
+        for i in range(len(z)-1):
+            if z[i]+100 != z[i+1]:
+                return False
+    else:
+        return False
+    
+    return max(z)
+
+for x in range(862346, 1056242+1):
+    if f(x):
+        print(x, f(x))
+    
+    
