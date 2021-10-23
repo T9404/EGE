57e259b2f5e11cefea34005126c6975bfde70e42
tomcat <69140932+T9404@users.noreply.github.com>
2021-10-23 16:31:53 +0500

    Create 2154.py

---
+++ Python/ЕГЭ2021;2022/25/Задания из kompege/2154.py
@@ -0,0 +1,23 @@
+def prime(x):
+    return all(x % i != 0 for i in range(2, int(x**0.5)+1))
+
+
+def f(x):
+    a = set()
+    for i in range(2, int(x**0.5)+1):
+        if x % i == 0:
+            a.add(i)
+            a.add(x//i)
+    b = list(a)
+    b = [i for i in b if prime(i)]
+    
+    if 400 < len(a) < 440 and len(b) >= 1:
+        return max(b)
+    else:
+        return 0
+    
+
+for i in range(5000000, 10000000+1):
+    if i % 180 == 0:
+        if f(i):
+            print(i, f(i))
