660c7434b684aa9b8aba7c71d0b31a4bd916e675
tomcat <69140932+T9404@users.noreply.github.com>
2021-11-07 19:00:45 +0500

    Update 971.py

--- Python/ЕГЭ2021;2022/5/Задания из kompege/971.py
+++ Python/ЕГЭ2021;2022/5/Задания из kompege/971.py
@@ -1,15 +1,12 @@
 def f(x):
-    if  10000 > x > 999:
-        x = str(x)
-        d = [ int(x[0]), int(x[1]), int(x[2]), int(x[3]) ]
-        result = str(d[2]*d[3]) + str(d[0] * d[1])
-        return int(result)
-    else:
-        return None
+    x = str(x)
+    d = [ int(x[0]), int(x[1]), int(x[2]), int(x[3]) ]
+    result = str(d[2]*d[3]) + str(d[0] * d[1])
+    return int(result)
 
 #print(f(5431))
 
-for i in range(1000, 9999):
+for i in range(1000, 10000):
     if f(i) == 1214:
         answer = i
 print(answer)
