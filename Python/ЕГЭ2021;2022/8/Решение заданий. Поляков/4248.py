60247080d754a1eb800f4355848b7e3b1c5cc175
tomcat <69140932+T9404@users.noreply.github.com>
2021-11-18 18:48:10 +0500

    update

---
+++ Python/ЕГЭ2021;2022/8/Решение заданий. Поляков/4248.py
@@ -0,0 +1,12 @@
+from itertools import product
+
+k = 0
+
+for j in product('ЗЕРКАЛО', repeat=6):
+    word = ''.join(j)
+    if word.count('К'):
+        if word.count('К') <= 4:
+            if len(set(word)) == len(word) - word.count('К') + 1: 
+                k += 1 # кол-во = кол-во общее + 1 (наше К) - кол-во К (повторов)
+
+print(k)
---
+++ Python/ЕГЭ2021;2022/8/Решение заданий. Поляков/4254.py
@@ -0,0 +1,9 @@
+from itertools import product
+
+k = 0
+for s in set(product('01234', repeat=5)):
+    num = ''.join(s)
+    if ( num.count('0') + num.count('2') + num.count('4') ) <= 3 and num[0] != '0':
+        k+=1 # число с нуля не начинается
+
+print(k)
