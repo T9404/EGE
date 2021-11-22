bb64eace23fe6c77f213d985ec937f6bbe2b5432
Malenago <87613601+Malenago@users.noreply.github.com>
2021-11-22 18:46:50 +0300

    Create 1231.py

---
+++ Python/ЕГЭ2021;2022/25/Задания из kompege/1231.py
@@ -0,0 +1,19 @@
+def F(n):
+    a=set()
+    for i in range(2,int(n**0.5)+1):
+        if (n%i==0): a.add(i); a.add(n//i)
+
+    if (len(a)!=0) and ((max(a)+min(a))%123==17):
+        return max(a)+min(a)
+    else: return 0
+
+c=0
+for i in range(250201,10000000000,1):
+    if (F(i)!=0):
+        c+=1
+        if (c<=5):
+            print(i,F(i))
+        else: break
+
+
+
