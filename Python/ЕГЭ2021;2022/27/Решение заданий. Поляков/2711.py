b8ab5b2412b68d1825c369fa79fe88ee8c8ad24a
Malenago <87613601+Malenago@users.noreply.github.com>
2021-12-12 14:24:32 +0300

    Create 2711.py

---
+++ Python/ЕГЭ2021;2022/27/Решение заданий. Поляков/2711.py
@@ -0,0 +1,13 @@
+import math
+f=open('C:\\Users\\XiaoMai\\Downloads\\27-36b.txt')
+n=int(f.readline())
+
+s=[0]
+for _ in range(n):
+    a=[int(x) for x in f.readline().split()]
+    cmb=[math.gcd(a[0],a[1]),math.gcd(a[0],a[2]),math.gcd(a[2],a[1])]
+    cmb2=[a+b for a in s for b in cmb]
+    s={x%10:x for x in sorted(cmb2)}.values()
+
+m=max(x for x in s if (x%10==0))
+print(m)
