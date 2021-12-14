5329d23f2f65d9609fea7d089fd2fb4cc26d0308
Malenago <87613601+Malenago@users.noreply.github.com>
2021-12-14 20:31:51 +0300

    Create 4172.py

---
+++ Python/ЕГЭ2021;2022/16/Решение заданий. Поляков/4172.py
@@ -0,0 +1,12 @@
+from functools import  lru_cache
+@lru_cache(None)
+def F(n):
+    if n<=3: return n+3
+    if (n>3) and (F(n-1)%2==0): return F(n-2)+n
+    if (n>3) and (F(n-1)%2==1): return F(n-2)+2*n
+
+s=0
+for i in range(40,51):
+    s+=F(i)
+
+print(s)
