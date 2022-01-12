b237d7478800547716fe4774121c00ab0df5e472
tomcat <69140932+T9404@users.noreply.github.com>
2022-01-12 20:20:44 +0500

    solving 23 tasks

---
+++ Python/ЕГЭ2021;2022/23/Решение заданий. Поляков/4104.py
@@ -0,0 +1,12 @@
+def f(s, e):
+    if s > e:
+        return 0
+    elif s == e:
+        return 1
+    else:
+        if s % 2 == 0:
+            return f(s+1, e)+f(s*2, e)+f(s+1, e)
+        else:
+            return f(s+1, e)+f(s*2, e)+f(s+2, e)
+
+print(f(3, 9)*f(9, 17)*f(17, 25))
