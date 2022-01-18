26821681b85661da536532bf7e648d89851bc7a8
tomcat <69140932+T9404@users.noreply.github.com>
2022-01-18 16:56:06 +0500

    4749.py

---
+++ Python/ЕГЭ2021;2022/25/Решение заданий. Поляков/4749.py
@@ -0,0 +1,26 @@
+def f(n):
+    a = set()
+    for i in range(2, int(n**0.5)+1):
+        if n % i == 0:
+            a|={i, n//i}
+    if len(a) < 3:
+        return False
+    else:
+        return sum(sorted(a)[-3:])
+
+def check(a):
+    for i in range(len(a)-1):
+        if int(a[i]) > int(a[i+1]):
+            return False
+    return True
+
+i = 10_000_000
+k = 0
+while k < 5:
+    i += 1
+    s = f(i)
+    if s:
+        d = list(str(s))
+        if check(d):
+            k += 1
+            print(s)
