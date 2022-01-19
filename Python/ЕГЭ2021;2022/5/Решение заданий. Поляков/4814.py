2283d819f7ead29b89da9b4546824acfad9600ee
tomcat <69140932+T9404@users.noreply.github.com>
2022-01-19 20:19:08 +0500

    solutions of part 1

---
+++ Python/ЕГЭ2021;2022/5/Решение заданий. Поляков/4814.py
@@ -0,0 +1,16 @@
+def find_s2(n):
+    n_2 = str(n)[::-1]
+    a = ''
+    for i in range(0, len(n_2), 2):
+        a += n_2[i]
+    return sum(map(int, a))
+
+def f(n):
+    s1 = sum([int(i) for i in str(n) if int(i) % 2 == 0 ])
+    s2 = find_s2(n)
+    return abs(s1-s2)
+
+for i in range(1, 10000000):
+    if f(i) == 26:
+        print(i)
+        break
