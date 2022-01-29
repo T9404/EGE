e53da6c4bf46c3489ae74f47be7d846db6679014
tomcat <69140932+T9404@users.noreply.github.com>
2022-01-29 09:00:50 +0500

    Update СортировкаВыбором.cpp

--- C++/Алгоритмы/СортировкаВыбором.cpp
+++ C++/Алгоритмы/СортировкаВыбором.cpp
@@ -1,32 +1,25 @@
-﻿#include <iostream> 
-#include <algorithm> 
+#include <iostream>
+#include <algorithm>
 
-int main()
-{
-	int const length = 5;
-	int array[length] = { 300, 510, 100000, 0, 40 };
+void selection_sort(int *a, int n) {
+    for (int i = 0; i < n-1; i++) {
+        for (int j = i+1; j < n; j++) 
+        {
+            if (a[i] > a[j]) 
+                std::swap(a[i], a[j]);
+        }
+    }
 
+}
 
-	for (int startIndex = 0; startIndex < length - 1; ++startIndex)
-	{
+int main() {
 
+    int a[5] = {5, 52, 11, 3, 1};
 
-		int smallestIndex = startIndex;
+    selection_sort(a, 5);
 
+    for (int i = 0; i < 5; i++)
+        std::cout << a[i] << " ";
 
-		for (int currentIndex = startIndex + 1; currentIndex < length; ++currentIndex)
-		{
-
-			if (array[currentIndex] < array[smallestIndex])
-
-				smallestIndex = currentIndex;
-		}
-
-		std::swap(array[startIndex], array[smallestIndex]);
-	}
-
-	for (int index = 0; index < length; ++index)
-		std::cout << array[index] << ' ';
-
-	return 0;
+    return 0;
 }
