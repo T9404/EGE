0746981e4776bc2e13ca33e3bb8b99fe5bfadf06
tomcat <69140932+T9404@users.noreply.github.com>
2022-01-29 09:10:57 +0500

    Update Бинарный поиск.cpp

--- C++/Алгоритмы/Бинарный поиск.cpp
+++ C++/Алгоритмы/Бинарный поиск.cpp
@@ -1,39 +1,35 @@
-﻿#include <iostream>
+#include <iostream>
+#include <algorithm>
 
-int binsearch(int x, int v[], int n)
+int bin_search(int x, int v[], int n)
 {
     int low, high, mid;
-
+ 
     low = 0;
     high = n;
-    while (low <= high) {
-        mid = (low + high) / 2;
-        if (x < v[mid])
+    while(low <= high) {
+        mid = (low + high) / 2; 
+        if(x < v[mid])              
             high = mid - 1;
-        else if (x > v[mid])
+        else if(x > v[mid])
             low = mid + 1;
         else
             return mid;
     }
     return -1;
 }
+ 
 
-int main()
-{
-    int i, ret, num, nsize = 6, a[6];
+int main() {
 
-    /* заполнение массива */
-    for (i = 0; i < nsize; i++) {
-        a[i] = i;
-        printf("%d ", a[i]);
-    }
+    int a[5] = {1, 2, 3, 4, 5};
 
-    num = 1;
+    int value = bin_search(12, a, 5);
 
-    /* поиск числа num в массиве a */
-    if ((ret = binsearch(num, a, nsize)) < 0)
-        printf("\n Not found ", num);
+    if (value != -1)
+        std::cout << "Index: " << value;
     else
-        printf("\n %d in array \n", num, ret);
+        std::cout << "Nothing found";
+
     return 0;
 }
