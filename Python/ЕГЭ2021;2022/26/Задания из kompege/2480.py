77f5d9478ab7bc273de72916d8e4caa9ef9ad0ff
tomcat <69140932+T9404@users.noreply.github.com>
2022-02-01 15:22:03 +0500

    new solutions to 26 tasks

---
+++ Python/ЕГЭ2021;2022/26/Задания из kompege/2480.py
@@ -0,0 +1,29 @@
+f = open('2480.txt') 
+n = int(f.readline())
+
+road = [0]*2_000_001 #из условия <2_000_000
+
+for i in range(n):
+    a, b = map(int, f.readline().split())
+    road[a] += 1
+    road[b] -= 1
+
+
+k = road[0]
+update = [] #дорога с жалобами
+for i in range(1, len(road)):
+    update.append(k)
+    k += road[i]
+
+
+count_road = len_roads = 0
+
+for i in range(1, len(update)):
+    if update[i-1] == 0 and update[i] >0:
+        start = i
+    if update[i-1] > 0 and update[i] == 0:
+        count_road += 1
+        len_roads += (i - start)
+
+
+print(count_road, len_roads)
