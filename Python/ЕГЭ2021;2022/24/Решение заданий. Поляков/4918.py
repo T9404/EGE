d5aff328f34efb9502df0668efcd486f27cbc34d
Malenago <87613601+Malenago@users.noreply.github.com>
2022-02-20 09:34:59 +0300

    new

---
+++ Python/ЕГЭ2021;2022/24/Решение заданий. Поляков/4918.py
@@ -0,0 +1,6 @@
+f=open('C:\\Users\\XiaoMai\\Downloads\\24-191.txt')
+s=str(f.readline())
+
+s=s.replace('A','A A',s.count('A')).replace('B','B B',s.count('B'))
+
+print(len([x for x in s.split() if x[0]=='A' and x[len(x)-1]=='B' and x.count('F')>=1 and x.count('A')==1 and x.count('B')==1 and len(x)<=15]))
