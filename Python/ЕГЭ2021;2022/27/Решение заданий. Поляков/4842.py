8c570ebdf1838aff4dfa92157d6ae57ba6ef681d
Леонид Дмитриевич Шастин <87826460+LL10N2020K@users.noreply.github.com>
2022-03-14 23:22:03 +0300

    Add files via upload

---
+++ Python/ЕГЭ2021;2022/27/Решение заданий. Поляков/4842.py
@@ -0,0 +1,6 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+==================== RESTART: C:\Users\lenya\Desktop\git.py ====================
+7376668
+166798285
