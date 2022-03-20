e021f3863af67a37d6995c7881bdaf8c6569f78b
T9404 <lelik-serzh@bk.ru>
2022-03-20 11:43:16 +0500

    tiger

--- C++/ЕГЭ2021;2022/19-21/Решение заданий. Kompege/2369.cpp
+++ C++/ЕГЭ2021;2022/19-21/Решение заданий. Kompege/2369.cpp
@@ -1,9 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 #define x first.first
 #define y first.second
 #define z second
+
 vector<int> delta = {3, 13, 23};
+
 vector<pair<pair<int, int>, int>> m(pair<pair<int, int>, int> h)
 {
     vector<pair<pair<int, int>, int>> res;
@@ -15,7 +18,9 @@ vector<pair<pair<int, int>, int>> m(pair<pair<int, int>, int> h)
     }
     return res;
 }
+
 map<pair<pair<int, int>, int>, string> mp;
+
 string g(pair<pair<int, int>, int> h)
 {
     if (mp.count(h) == 0)
@@ -25,15 +30,19 @@ string g(pair<pair<int, int>, int> h)
         else
         {
             vector<pair<pair<int, int>, int>> M = m(h);
+            
             if (any_of(M.begin(), M.end(), [](pair<pair<int, int>, int> p)
                        { return g(p) == "w"; }))
                 mp[h] = "p1";
+
             else if (all_of(M.begin(), M.end(), [](pair<pair<int, int>, int> p)
                             { return g(p) == "p1"; }))
                 mp[h] = "v1";
+
             else if (any_of(M.begin(), M.end(), [](pair<pair<int, int>, int> p)
                             { return g(p) == "v1"; }))
                 mp[h] = "p2";
+
             else if (all_of(M.begin(), M.end(), [](pair<pair<int, int>, int> p)
                             { return g(p) == "p1" or g(p) == "p2"; }))
                 mp[h] = "v2";
@@ -41,8 +50,10 @@ string g(pair<pair<int, int>, int> h)
     }
     return mp[h];
 }
+
 int main()
 {
+
     for (int i = 1; i <= 23; i++) //№19 - проверка с any_of: строка 31; запускать отдельно
     {
         pair<pair<int, int>, int> p = {{2, i}, 2 * i};
@@ -51,6 +62,7 @@ int main()
             cout << i << ": " << g(p) << "\n";
         }
     }
+
     for (int i = 1; i <= 23; i++) //№20
     {
         pair<pair<int, int>, int> p = {{2, i}, 2 * i};
@@ -59,6 +71,7 @@ int main()
             cout << i << ": " << g(p) << "\n";
         }
     }
+
     for (int i = 1; i <= 23; i++) //№21
     {
         pair<pair<int, int>, int> p = {{2, i}, 2 * i};
@@ -67,4 +80,6 @@ int main()
             cout << i << ": " << g(p) << "\n";
         }
     }
+
+    return 0;
 }
