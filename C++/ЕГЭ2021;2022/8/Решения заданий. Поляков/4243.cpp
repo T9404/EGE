75eefe612926d4f9e375004501d897ad5e61565f
Minor1o <mramirabdullin@gmail.com>
2022-03-22 19:40:39 +0300

    №8 adding solution
    Added combinatorics solution

---
+++ C++/ЕГЭ2021;2022/8/Решения заданий. Поляков/4243.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+#define npos string::npos
+using namespace std;
+
+bool check(string s)
+{
+    set<char> c;
+    for (auto &i : s)
+        c.insert(i);
+    return (c.size() == s.size() && s[0] != 'I' && s.find("VF") == npos && s.find("FV") == npos);
+}
+
+int main()
+{
+    int c = 0;
+    set<string> a;
+    string s = "VAIFU";
+    for (int i1 = 0; i1 < 5; i1++)
+        for (int i2 = 0; i2 < 5; i2++)
+            for (int i3 = 0; i3 < 5; i3++)
+                for (int i4 = 0; i4 < 5; i4++)
+                {
+                    string p = {s[i1], s[i2], s[i3], s[i4]};
+                    if (check(p))
+                    {
+                        c++;
+                        a.insert(p);
+                    }
+                }
+    cout << c << " " << a.size();
+}
