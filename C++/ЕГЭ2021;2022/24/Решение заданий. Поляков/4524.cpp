b0160440d78f015d6cee376f39ce0844626e097b
Minor1o <mramirabdullin@gmail.com>
2022-03-24 17:33:07 +0300

    Split with any symbol
    Overloaded split by getline without iterators

---
+++ C++/ЕГЭ2021;2022/24/Решение заданий. Поляков/4524.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<string> split(string &s, char d)
+{
+    vector<string> res;
+    string t;
+    istringstream istr(s);
+    while (getline(istr, t, d))
+        res.push_back(t);
+    return res;
+}
+int main()
+{
+    fstream f;
+    f.open("4524.txt");
+    string s;
+    f >> s;
+    vector<string> res = split(s, '.');
+    string m = "";
+    for (int i = 1; i < res.size(); i++)
+    {
+        if (res[i - 1].size() + 1 + res[i].size() > m.size())
+            m = res[i - 1] + '.' + res[i];
+    }
+    cout << m << "\n" << m.size();
+}
