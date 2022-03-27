d7426f227e44c0cbcaf0fe3623d2cd38e27617f9
Minor1o <mramirabdullin@gmail.com>
2022-03-27 14:46:59 +0300

    №26 solution update

---
+++ C++/ЕГЭ2021;2022/26/Решение заданий. Kompege/1395.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    fstream f;
+    f.open("1395.txt");
+    int s, n;
+    f >> s >> n;
+    vector<int> a(n);
+    for (auto &e : a)
+        f >> e;
+    sort(a.begin(), a.end());
+    int sum = 0, count = 0;
+    for (int i = 0; i < a.size(); i++)
+    {
+        if (sum + a[i] <= s)
+        {
+            sum += a[i], count++;
+        }
+        else
+            break;
+    }
+    cout << n - count << " " << accumulate(a.begin(), a.end(), -sum);
+}
