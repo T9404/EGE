19ce8e4df105c6e3cf36adde527b8433cc452025
Minor1o <mramirabdullin@gmail.com>
2022-03-27 13:57:30 +0300

    №26 solutions update

---
+++ C++/ЕГЭ2021;2022/26/Решение заданий. Kompege/2149.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    fstream f;
+    f.open("2149.txt");
+    int n, m;
+    f >> n >> m;
+    vector<int> img, vid;
+    int x;
+    while (n--)
+    {
+        f >> x;
+        if (x <= 100)
+            img.push_back(x);
+        else
+            vid.push_back(x);
+    }
+
+    sort(img.begin(), img.end()), sort(vid.begin(), vid.end());
+
+    vector<int> vpr(vid.size() + 1);
+    vpr[1] = vid[0];
+    for (int i = 2; i <= vid.size(); i++)
+        vpr[i] = vpr[i - 1] + vid[i - 1];
+
+    vector<int> mpr(img.size() + 1);
+    mpr[1] = img[0];
+    for (int i = 2; i <= img.size(); i++)
+        mpr[i] = mpr[i - 1] + img[i - 1];
+
+    int count = 0;
+    for (int i = 1; i <= vid.size(); i++)
+    {
+        if (vpr[i] >= (m / 2))
+        {
+            m -= vpr[i];
+            count += i;
+            break;
+        }
+    }
+    int sumimg;
+    for (int i = 1; i <= img.size(); i++)
+    {
+        if (mpr[i] > m)
+        {
+            count += (i - 1);
+            sumimg = mpr[i - 1];
+            break;
+        }
+    }
+    for (int i = img.size() - 1; i >= 0; i--)
+    {
+        if (sumimg + img[i] <= m)
+        {
+            m -= (sumimg + img[i]);
+            break;
+        }
+    }
+    cout << m << " " << count;
+}
