8af4011236407247a6f4c4d22030f59df49318ea
tomcat <69140932+T9404@users.noreply.github.com>
2022-03-29 07:35:12 +0500

    Update 4862.py

--- Python/ЕГЭ2021;2022/19-21/Решение заданий. Поляков/4862.py
+++ Python/ЕГЭ2021;2022/19-21/Решение заданий. Поляков/4862.py
@@ -5,20 +5,20 @@ from itertools import product
 def move(e):
     # h - список карт, l - последняя карта противника,
     # s - начальная карта всей партии,
-    # flag - первая карта? усл: "Первая карта может быть любой из доступных."
+    # flag - самая первая карта битвы? тогда мы должны убрать именно её
 
     h, l, s, flag = sorted(e[:-3]), e[-3], e[-2], e[-1]
 
     new_h = []
 
-    if not flag: 
+    if flag: # если начало битвы
 
         m = tuple(h[h.index(l)+1:])
-        new_h.append((*m, l, s, True))
+        new_h.append((*m, l, s, False))
 
-        return new_h # убираем самую первую карту
+        return new_h # убираем карту l, с которой начали битву
 
-    else:
+    else: 
 
         # значение карт может повторяться. убираем лишнее
         uniq_cards = set()
@@ -26,7 +26,7 @@ def move(e):
         for i in range(len(h)):
 
             # выбираем карту, которая подходит под условие
-            if ((l+1 == h[i]) or (l == h[i])) and h[i] not in uniq_cards: 
+            if ((l+1 == h[i]) or (l == h[i])) and (h[i] not in uniq_cards): 
 
                 m = tuple(h[h.index(h[i])+1:])
                 uniq_cards.add(h[i])
@@ -85,7 +85,7 @@ def f(h):
 
 cards_19 = (7, 8, 8, 8, 9, 9)
 
-arr19 = [i for i in set(cards_19) if f((*cards_19, i, i, False))[0] == 'P']
+arr19 = [i for i in set(cards_19) if f((*cards_19, i, i, True))[0] == 'P']
 print(*arr19)
 
 
@@ -93,13 +93,13 @@ print(*arr19)
 
 cards_20 = (5, 6, 6, 7, 7, 7, 8, 9, 9, 9, 10, 10)
 
-arr20 = [i for i in set(cards_20) if f((*cards_20, i, i, False))[0] == 'P']
+arr20 = [i for i in set(cards_20) if f((*cards_20, i, i, True))[0] == 'P']
 print(min(arr20), max(arr20))
 
 
 
 
-# массив с картами для набора
+# массив с картами набора
 cards_21 = [[i]*4 for i in range(4, 7+1)] 
 answer_21 = 0
 
@@ -107,7 +107,7 @@ for i, j, m, u in product(range(4), repeat=4):
 
     q = tuple(cards_21[0][:i+1] + cards_21[1][:j+1] + cards_21[2][:m+1] + cards_21[3][:u+1])
 
-    if all(f((*q, i, i, False))[0] == 'V' for i in range(4, 7+1)): 
+    if all(f((*q, i, i, True))[0] == 'V' for i in range(4, 7+1)): 
         answer_21 += 1
 
 print(answer_21)
