f768259389def6135a52fa7219d807ce2f00b857
Malenago <87613601+Malenago@users.noreply.github.com>
2022-04-03 14:59:09 +0300

    Create 4204.py

---
+++ Python/ЕГЭ2021;2022/27/Решение заданий. Поляков/4204.py
@@ -0,0 +1,23 @@
+f=open('C:\\Users\\XiaoMai\\Downloads\\27-70a.txt')
+n=int(f.readline())
+
+s1,s2=0,0
+ost=[float('inf')]*5
+
+for _ in range(n):
+    a,b=map(int,f.readline().split())
+    s1+=min(a,b)
+    s2+=max(a,b)
+    ost[(max(a,b)-min(a,b))%5]=min(max(a,b)-min(a,b),ost[(max(a,b)-min(a,b))%5])
+
+ma=0
+for x in ost:
+    n=(s2-x)-(s1+x)
+    if n%5==0:
+        ma=max(ma,n)
+
+print(ma)
+
+
+
+
