4f7ab6b9e18db5ecb480c4e09ae4aec1fdfa7d3d
Malenago <87613601+Malenago@users.noreply.github.com>
2022-04-03 15:29:04 +0300

    Create 4132.py

---
+++ Python/ЕГЭ2021;2022/26/Решение заданий. Поляков/4132.py
@@ -0,0 +1,34 @@
+f=open('C:\\Users\\XiaoMai\\Downloads\\26-56.txt')
+v,k,n=map(int,f.readline().split())
+
+a=[int(x) for x in f]
+a.sort(reverse=True)
+
+disk=[0]*k
+otl=[]
+
+p=''
+for i in range(k):
+    p+=str(i)
+
+for x in a:
+    c=0
+    for i in p:
+        i=int(i)
+        c+=1
+        if (disk[i]+x)<=v:
+            disk[i]+=x
+            p=p[1:len(p)]+p[0]
+            break
+        elif c==k:
+             otl.append(x)
+
+print(sum(otl),len(otl))
+
+
+
+
+
+
+
+
