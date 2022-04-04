279b6c86051c313ede235740c7b851b84db05bab
T9404 <lelik-serzh@bk.ru>
2022-04-04 18:50:59 +0500

    refactoring

--- Python/ЕГЭ2021;2022/19-21/Задания из kompege/2364.py
+++ Python/ЕГЭ2021;2022/19-21/Задания из kompege/2364.py
@@ -1,42 +1,27 @@
-from functools import lru_cache
+def f(a, c, m):
+    if a > 20:
+        return c % 2 == m % 2
+    if c == m:
+        return 0
+
+    h = [f(a+1, c+1, m), f(a+2, c+1, m), f(a+3, c+1, m)]
+
+    return any(h) if (c+1) % 2 == m % 2 else all(h)
 
 
-def move(h):
-    a, b = h
-    
-    ans = []  # записываем ходы в массив
-    
-    if a-1 > 0:
-        ans.append((a-1, b))
-    if b-1 > 0:
-        ans.append((a, b-1))
-    if a != 1:
-        ans.append((int(a//2), b))
-    if b != 1:
-        ans.append((a, int(b//2)))
-        
-    return ans  # возвращаем массив
+t19, t20, t21 = [], [], set()
+
+for a in range(1, 21):
+    for m in range(1, 15):
+        if f(a, 0, m) == 1:
+            if m == 2:
+                t19.append(a)
+            if m == 5:
+                t20.append(a)
+            if m % 2 == 0:
+                t21.add(a)
 
 
-@lru_cache(None)
-def f(h):
-    if sum(h) <= 18:
-        return 'CP'
-    elif any(f(m) == 'CP' for m in move(h)):
-        return 'P1'
-    elif all(f(m) == 'P1' for m in move(h)):
-        return 'B1'
-    elif any(f(m) == 'B1' for m in move(h)):
-        return 'P2'
-    elif all(f(m) in ['P1', 'P2'] for m in move(h)):
-        return 'B2'
-
-
-task_19 = [m for m in range(1, 50) if f((m, m)) == 'B1']
-print(*task_19)
-
-task_20 = [s for s in range(1, 50) if f((s, 13)) == 'P2']
-print(task_20[0], task_20[-1])
-
-task_21 = [n for n in range(1, 50) if f((n, n)) == 'B2']
-print(min(task_21))
+print(min(t19))
+print(t20[0], t20[1], t20[2])
+print(len(t21))
--- Python/ЕГЭ2021;2022/27/Решение заданий. Поляков/4204.py
+++ Python/ЕГЭ2021;2022/27/Решение заданий. Поляков/4204.py
@@ -1,23 +1,24 @@
-f=open('C:\\Users\\XiaoMai\\Downloads\\27-70a.txt')
-n=int(f.readline())
+f = open('27-70a.txt')
+n = int(f.readline())
 
-s1,s2=0,0
-ost=[float('inf')]*5
+
+ost = [float('inf')]*5
+s1, s2 = 0, 0
+
 
 for _ in range(n):
-    a,b=map(int,f.readline().split())
-    s1+=min(a,b)
-    s2+=max(a,b)
-    ost[(max(a,b)-min(a,b))%5]=min(max(a,b)-min(a,b),ost[(max(a,b)-min(a,b))%5])
+    a, b = map(int, f.readline().split())
+    s1 += min(a, b)
+    s2 += max(a, b)
+    ost[(max(a, b)-min(a, b)) % 5] = min(max(a, b) -
+                                         min(a, b), ost[(max(a, b)-min(a, b)) % 5])
 
-ma=0
+
+ma = 0
+
 for x in ost:
-    n=(s2-x)-(s1+x)
-    if n%5==0:
-        ma=max(ma,n)
+    n = (s2-x)-(s1+x)
+    if n % 5 == 0:
+        ma = max(ma, n)
 
 print(ma)
-
-
-
-
