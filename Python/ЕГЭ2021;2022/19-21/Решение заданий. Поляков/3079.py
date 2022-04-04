20fa0eeb49b1f0249d8cfcb6c60b9c1e18ae8ab7
Malenago <87613601+Malenago@users.noreply.github.com>
2022-04-04 16:59:26 +0300

    Create 3079.py

---
+++ Python/ЕГЭ2021;2022/19-21/Решение заданий. Поляков/3079.py
@@ -0,0 +1,25 @@
+def f(a,c,m):
+    if a>33: return c%2==m%2
+    if c==m: return 0
+
+    h=[f(a+1,c+1,m),f(a+2,c+1,m),f(a+3,c+1,m),f(a*2,c+1,m)]
+
+    return any(h) if (c+1)%2==m%2 else all(h)
+
+t19=[]
+t20=[]
+t21=[]
+
+t1=[]
+
+for s in range(1,34):
+    for m in range(1,6):
+        if f(s,0,m)==1:
+            if m==1: t1.append(s)
+            if m==2: t19.append(s)
+            if m==3 and not s in t1: t20.append(s)
+            if m==4 and not s in t19: t21.append(s)
+
+print('19 - ',t19[0])
+print('20 - ',min(t20),max(t20))
+print('21 - ',t21[0])
