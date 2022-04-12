5f3aa5f79edcf677319f76072a8c5a8531188a4e
Леонид Дмитриевич Шастин <87826460+LL10N2020K@users.noreply.github.com>
2022-04-12 23:30:51 +0300

    Update 2109.py

--- Python/ЕГЭ2021;2022/27/Задания из kompege/2109.py
+++ Python/ЕГЭ2021;2022/27/Задания из kompege/2109.py
@@ -5,9 +5,8 @@ s = {}
 l = -1
 for i in range(n):
     x = int(f.readline())
-    c = [(sm + x, ln + 1, x, x - lst) for sm, ln, lst, r in s.values() if x - lst == r or r == 0] + [(x, 1, x, 0)]
+    c = [(sm + x, ln + 1, x, x - lst) for sm, ln, lst, r in s.values() if x - lst >= 1 and (x - lst == r or r == 0)] + [(x, 1, x, 0)]
     s = {x[0]%7: x for x in sorted(c, key = lambda x: x[1])}
     l = max(l, s.get(0, (0, 0))[1])
 
 print(l)
-
