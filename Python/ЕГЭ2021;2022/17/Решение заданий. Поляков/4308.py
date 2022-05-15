93774fec4647b1d7c519abb124addf0bf23a5a2e
tomcat <69140932+T9404@users.noreply.github.com>
2022-05-15 18:28:26 +0500

    Update 4308.py

--- Python/ЕГЭ2021;2022/17/Решение заданий. Поляков/4308.py
+++ Python/ЕГЭ2021;2022/17/Решение заданий. Поляков/4308.py
@@ -8,11 +8,10 @@ k, mik = 0, 0
 
 
 for i in range(1, len(d)-2):  # с 0, 1 и 2, 3го элементов начало
-    a = int(d[i-1])
-    b = int(d[i])
-    c = int(d[i+1])
-    e = int(d[i+2])  # a, b, c, e будут с теми же знаками!
-
+    
+    # a, b, c, e будут с теми же знаками
+    a, b, c, e = int(d[i-1]),  int(d[i]), int(d[i+1]), int(d[i+2])  
+    
     if (abs(a) % 2 != abs(b) % 2):
         if (abs(b) % 2 != (abs(c) % 2)):
             if (abs(c) % 2 != abs(e) % 2):
