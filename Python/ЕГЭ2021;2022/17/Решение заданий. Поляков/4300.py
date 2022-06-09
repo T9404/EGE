2c133933b4655bc5abd8a7c5d7aeb96e63b9db0f
tomcat <69140932+T9404@users.noreply.github.com>
2022-06-09 22:46:54 +0500

    Update 4300.py

--- Python/ЕГЭ2021;2022/17/Решение заданий. Поляков/4300.py
+++ Python/ЕГЭ2021;2022/17/Решение заданий. Поляков/4300.py
@@ -1,19 +1,14 @@
 f = open('17.txt')
 n = 5000
 
-
 a = [int(f.readline()) for _ in range(n)]
 k, mak = 0, 0
 
-
 for i in range(1, n):
-    if (a[i-1] + a[i]) % 3 == 0:
-
-        if (a[i-1]+a[i]) % 6 != 0:
-            
-            if abs(a[i]*a[i-1]) % 10 == 8:
-                mak = max(mak, a[i-1]+a[i])
-                k += 1
-
+    if ((a[i-1] + a[i]) % 3 == 0 and
+            (a[i-1]+a[i]) % 6 != 0 and
+            abs(a[i] * a[i-1]) % 10 == 8):
+        mak = max(mak, a[i-1]+a[i])
+        k += 1
 
 print(k, mak)
